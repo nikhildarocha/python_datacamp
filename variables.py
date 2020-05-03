43351b24cce612511172825254602debe01c6771
nikhildarocha <niks77883@gmail.com>
2020-05-03 19:14:47 +0530

    more variables lol

--- variables.py
+++ variables.py
@@ -9,3 +9,25 @@ result = savings*growth_multiplier**7
 
 # Print out result
 print(result)
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of growth_multiplier and savings to year1
+
+year1 = savings*growth_multiplier
+# Print the type of year1
+
+print(type(year1))
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
