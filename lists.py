3fffff766b1767a5cfc450363d26c1f5456951ae
nikhildarocha <niks77883@gmail.com>
2020-05-05 01:40:35 +0530

    addition subnetting

--- lists.py
+++ lists.py
@@ -42,3 +42,12 @@ print(areas[9])
 
 # Print out the area of the living room
 print(areas[5])
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+
+eat_sleep_area = areas[3] + areas[7]
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
