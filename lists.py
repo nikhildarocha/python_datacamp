a5d343b12d7a222e91a73544a1dab0c8ad4b5ef1
nikhildarocha <niks77883@gmail.com>
2020-05-09 21:31:16 +0530

    @ndarocha lists

--- lists.py
+++ lists.py
@@ -74,3 +74,13 @@ areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.7
 downstairs = areas[:6]
 # Alternative slicing to create upstairs
 upstairs = areas[6:]
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+
+areas[-1] = 10.50
+# Change "living room" to "chill zone"
+areas[4] = "chill zone"
