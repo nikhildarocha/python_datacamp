83d05314f506693e93ea8bcf422bc2b77a7d10ed
nikhildarocha <niks77883@gmail.com>
2020-05-10 00:11:28 +0530

    @ndarocha numpy

--- functions.py
+++ functions.py
@@ -81,3 +81,16 @@ A = math.pi*r*r
 # Build printout
 print("Circumference: " + str(C))
 print("Area: " + str(A))
+
+
+# Definition of radius
+r = 192500
+
+# Import radians function of math package
+
+from math import radians
+# Travel distance of Moon over 12 degrees. Store in dist.
+
+dist = r*radians(12)
+# Print out dist
+print(dist)
