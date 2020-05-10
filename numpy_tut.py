61c5ca440d56cd524ce188f19d7209fdef07cc95
nikhildarocha <niks77883@gmail.com>
2020-05-10 08:08:31 +0530

    @ndarocha numpy

--- numpy_tut.py
+++ numpy_tut.py
@@ -9,3 +9,40 @@ np_baseball = np.array(baseball)
 
 # Print out type of np_baseball
 print(np_baseball)
+
+# height is available as a regular list
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height_in: np_height_in
+
+np_height_in = np.array(height_in)
+# Print out np_height_in
+
+print(np_height_in)
+# Convert np_height_in to m: np_height_m
+
+np_height_m = np_height_in*0.0254
+
+print(np_height_m)
+# Print np_height_m
+
+
+# height and weight are available as regular lists
+
+# Import numpy
+import numpy as np
+
+# Create array from height_in with metric units: np_height_m
+np_height_m = np.array(height_in) * 0.0254
+
+# Create array from weight_lb with metric units: np_weight_kg
+
+np_weight_kg = np.array(weight_lb)*0.453592
+# Calculate the BMI: bmi
+bmi = np_weight_kg/np_height_m**2
+print(bmi)
+
+# Print out bmi
+
